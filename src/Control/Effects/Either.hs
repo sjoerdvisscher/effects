9eeccf96a6e0bbbb00da01b9a67b30a4182d39c1
Sjoerd Visscher <sjoerd@w3future.com>
2011-10-17 14:33:41 +0200

    Simplified Either effect.

--- src/Control/Effects/Either.hs
+++ src/Control/Effects/Either.hs
@@ -4,11 +4,11 @@ module Control.Effects.Either where
 import Control.Effects
 import Data.Void
 
-throwEither :: (c ~ ContT (m (Either e r)) m, AutoLift c n, Monad m) => Proxy c -> e -> n Void
-throwEither p e = operation p $ \_ -> return . return $ Left e
+throwEither :: (c ~ ContT (Either e r) m, AutoLift c n, Monad m) => Proxy c -> e -> n Void
+throwEither p e = operation p $ \_ -> return $ Left e
 
-catchEither :: Monad m => (e -> m a) -> Handler (m (Either e a)) a m a
+catchEither :: Monad m => (e -> m a) -> Handler (Either e a) a m a
 catchEither h = Handler
-  { ret = return . return . return
-  , fin = \m -> m >>= either h return
+  { ret = return . return
+  , fin = either h return
   }
