fc79712e0edc3989e15e074894722acc09d7e80f
Sjoerd Visscher <sjoerd@w3future.com>
2011-11-12 15:40:03 +0100

    Added modify and local, and a State type.

--- src/Control/Effects/State.hs
+++ src/Control/Effects/State.hs
@@ -1,15 +1,30 @@
-{-# LANGUAGE MultiParamTypeClasses, TypeFamilies, FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Control.Effects.State where
 
 import Control.Effects
 
-get :: AutoLift (s -> m r) m n => Effect (s -> m r) m -> n s
+type State s m a = s -> m a
+
+get :: AutoLift (State s m a) m n => Effect (State s m a) m -> n s
 get p = operation p $ \k -> return $ \s -> do r <- k s; r s
 
-put :: AutoLift (s -> m r) m n => Effect (s -> m r) m -> s -> n ()
+put :: AutoLift (State s m a) m n => Effect (State s m a) m -> s -> n ()
 put p s' = operation p $ \k -> return $ \_ -> do r <- k (); r s'
 
-ref :: Monad m => s -> Handler (s -> m a) a m a
+modify :: AutoLift (State s m a) m n => Effect (State s m a) m -> (s -> s) -> n ()
+modify p f = do
+  v <- get p
+  put p (f v)
+
+local :: AutoLift (State s m a) m n => Effect (State s m a) m -> (s -> s) -> n b -> n b
+local p f m = do
+  v <- get p
+  put p (f v)
+  r <- m
+  put p v
+  return r
+
+ref :: Monad m => s -> Handler (State s m a) a m a
 ref s_init = Handler
   { ret = return . return . return
   , fin = \f -> f s_init
